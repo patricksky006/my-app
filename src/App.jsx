ac9d07c08b97ad86c498c7243b54933d3b9dc719
patricksky006 <patrick.sky@hotmail.com>
2023-11-18 19:54:59 +0800

    project init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import MainPage from './components/Mainpage'
+
+function App() {
+  return (
+    <>
+      <MainPage />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Mainpage.jsx
@@ -0,0 +1,7 @@
+export default function MainPage() {
+  return (
+    <>
+    <h1>hello, world</h1>
+    </>
+  )
+}
