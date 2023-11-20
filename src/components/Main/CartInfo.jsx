fec6074229de3404f20ecc9ba981b52c534d58f2
patricksky006 <patrick.sky@hotmail.com>
2023-11-20 21:20:54 +0800

    cartinfo.scss

--- src/components/Main/CartInfo.jsx
+++ src/components/Main/CartInfo.jsx
@@ -3,6 +3,7 @@ import Product1 from '../../assets/images/product-1.jpg'
 import Product2 from '../../assets/images/product-2.jpg'
 import Minus from '../../assets/icons/minus.svg'
 import Plus from '../../assets/icons/plus.svg'
+import '../../style/cartinfo.scss'
 
 export default function CartInfo () {
   return (
@@ -21,7 +22,7 @@ export default function CartInfo () {
                 <img src={Plus} alt="" className="product-control-plus" />
               </div>
             </div>
-            <div className="product-price">$3999</div>
+            <div className="product-price">3999</div>
           </div>
           <div className="products">
             <img src={Product2} alt="" className="img-container" />
@@ -33,14 +34,14 @@ export default function CartInfo () {
                 <img src={Plus} alt="" className="product-control-plus" />
               </div>
             </div>
-            <div className="product-price">$1299</div>
+            <div className="product-price">1299</div>
           </div>
         </div>
-        <div className="shipping-fee-container">
+        <div className="shipping-container">
           <div className="shipping-fee">運費</div>
           <div className="shipping-price">免費</div>
         </div>
-        <div className="shipping-subtotal-container">
+        <div className="shipping-container">
           <div className="shipping-subtotal">小計</div>
           <div className="shipping-subtotal-price">5298</div>
         </div>
