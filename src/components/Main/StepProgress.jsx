5473643f36cc6733c066245756028bc40635d457
patricksky006 <patrick.sky@hotmail.com>
2023-11-27 21:13:33 +0800

    StepProgress useState

--- src/components/Main/StepProgress.jsx
+++ src/components/Main/StepProgress.jsx
@@ -1,24 +1,27 @@
 import React from 'react'
 import '../../style/step-progress.scss'
 
-export default function StepProgress () {
+export default function StepProgress ({ step }) {
   return (
     <>
     <div className="stepper-panel">
       <h2 className="Checkout">結帳</h2>
       <div className="stepper-container">
         <div className="step">
-          <div className="circle-container"></div>
+          { step < 2 ? <div className="circle-container">1</div> : ''}
+          { step > 1 ? <div className="circle-container-checked"></div> : ''}
           <div className="label-container">寄送地址</div>
         </div>
         <div className="connect-line"></div>
         <div className="step">
-          <div className="circle-container"></div>
+          { step < 3 ? <div className="circle-container">2</div> : ''}
+          { step > 2 ? <div className="circle-container-checked"></div> : ''}
           <div className="label-container">運送方式</div>
         </div>
         <div className="connect-line"></div>
         <div className="step">
-          <div className="circle-container"></div>
+          { step < 4 ? <div className="circle-container">3</div> : ''}
+          { step > 3 ? <div className="circle-container-checked"></div> : ''}
           <div className="label-container">付款資訊</div>
         </div>
       </div>
