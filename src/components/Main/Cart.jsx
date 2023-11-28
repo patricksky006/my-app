804def356e099f9bb3182bb1d7b3876483aa7be1
patricksky006 <patrick.sky@hotmail.com>
2023-11-28 14:00:24 +0800

    Cart useState

--- src/components/Main/Cart.jsx
+++ src/components/Main/Cart.jsx
@@ -2,40 +2,33 @@ import React from 'react'
 import Minus from '../../assets/icons/minus.svg'
 import Plus from '../../assets/icons/plus.svg'
 
-export const items = [
-  {
-    id: '1',
-    name: '貓咪罐罐',
-    img: 'https://picsum.photos/300/300?text=1',
-    price: 100,
-    quantity: 2
-  },
-  {
-    id: '2',
-    name: '貓咪干干',
-    img: 'https://picsum.photos/300/300?text=2',
-    price: 200,
-    quantity: 1
-  }
-]
-
-export default function CartItems () {
+export default function CartItems ({ items, productQuantities, decreaseNumber, increaseNumber }) {
   return (
-    items.map(item => {
-      return (
+    <>
+      {items.map((item) => (
         <div className="products" key={item.id}>
           <img src={item.img} alt="" className="img-container" />
           <div className="product-info">
             <div className="product-name">{item.name}</div>
             <div className="product-control-container">
-              <img src={Minus} alt="" className="product-control-minus" />
-              <span className="product-counts">0</span>
-              <img src={Plus} alt="" className="product-control-plus" />
+              <img
+                src={Minus}
+                alt=""
+                className="product-control-minus"
+                onClick={() => decreaseNumber(item.id)}
+              />
+              <span className="product-counts">{productQuantities[item.id]}</span>
+              <img
+                src={Plus}
+                alt=""
+                className="product-control-plus"
+                onClick={() => increaseNumber(item.id)}
+              />
             </div>
           </div>
-          <div className="product-price">{item.price}</div>
+          <div className="product-total-price">{item.price * productQuantities[item.id]}</div>
         </div>
-      )
-    })
+      ))}
+    </>
   )
 }
--- src/components/Main/CartInfo.jsx
+++ src/components/Main/CartInfo.jsx
@@ -1,26 +1,79 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../../style/cartinfo.scss'
 import CartItems from './Cart'
 
+export const items = [
+  {
+    id: '1',
+    name: '貓咪罐罐',
+    img: 'https://picsum.photos/300/300?text=1',
+    price: 100,
+    quantity: 2
+  },
+  {
+    id: '2',
+    name: '貓咪干干',
+    img: 'https://picsum.photos/300/300?text=2',
+    price: 200,
+    quantity: 1
+  }
+]
+
 export default function CartInfo () {
+  const [productQuantities, setProductQuantities] = useState({
+    1: 0,
+    2: 0
+  })
+
+  const [totalPrice, setTotalPrice] = useState(0)
+
+  useEffect(() => { // 計算總價
+    const newTotalPrice = items.reduce((total, item) => {
+      return total + item.price * productQuantities[item.id]
+    }, 0)
+
+    setTotalPrice(newTotalPrice)
+  }, [productQuantities])
+
+  function decreaseNumber (itemId) {
+    if (productQuantities[itemId] > 0) {
+      setProductQuantities((prevQuantities) => ({
+        ...prevQuantities,
+        [itemId]: prevQuantities[itemId] - 1
+      }))
+    }
+  }
+
+  function increaseNumber (itemId) {
+    setProductQuantities((prevQuantities) => ({
+      ...prevQuantities,
+      [itemId]: prevQuantities[itemId] + 1
+    }))
+  }
+
   return (
     <>
-    <div className="cart-container">
-      <div className="cart-wrapper">
-        <h2 className="cart-title">購物籃</h2>
-        <div className="cart-goods">
-          <CartItems />
-        </div>
-        <div className="shipping-container">
-          <div className="shipping-fee">運費</div>
-          <div className="shipping-price">免費</div>
-        </div>
-        <div className="shipping-container">
-          <div className="shipping-subtotal">小計</div>
-          <div className="shipping-subtotal-price">5298</div>
+      <div className="cart-container">
+        <div className="cart-wrapper">
+          <h2 className="cart-title">購物籃</h2>
+          <div className="cart-goods">
+            <CartItems
+              items={items}
+              productQuantities={productQuantities}
+              decreaseNumber={decreaseNumber}
+              increaseNumber={increaseNumber}
+            />
+          </div>
+          <div className="shipping-container">
+            <div className="shipping-fee">運費</div>
+            <div className="shipping-price">免費</div>
+          </div>
+          <div className="shipping-container">
+            <div className="shipping-subtotal">小計</div>
+            <div className="shipping-subtotal-price">{totalPrice}</div>
+          </div>
         </div>
       </div>
-    </div>
     </>
   )
 }
